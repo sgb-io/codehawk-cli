d9a8269e59e41d5a3663664fb3c962a8dc150606
Sam Brown <sam@sgb.io>
2020-06-15 23:06:18 +0100

    Rework linting and fix files

--- src/coverage.ts
+++ src/coverage.ts
@@ -1,23 +1,23 @@
-import fs from "fs"
-import { CoverageMapping, CoverageSummary } from "./types"
+import fs from 'fs'
+import { CoverageMapping, CoverageSummary } from './types'
 
-export const getCoverage = (dirPath: string): Array<CoverageMapping> => {
-    let coverage: Array<CoverageMapping> = []
+export const getCoverage = (dirPath: string): CoverageMapping[] => {
+  let coverage: CoverageMapping[] = []
 
-    try {
-        const contents = fs.readFileSync(`${dirPath}/coverage/coverage-summary.json`, 'utf8')
-        const coveredFiles: CoverageSummary = JSON.parse(contents)
-        coverage = Object.keys(coveredFiles).map((file) => ({
-            path: file.replace(dirPath, ''),
-            coverage: coveredFiles[file]
-        }))
-    } catch (e) {
-        console.error(`
+  try {
+    const contents = fs.readFileSync(`${dirPath}/coverage/coverage-summary.json`, 'utf8')
+    const coveredFiles: CoverageSummary = JSON.parse(contents)
+    coverage = Object.keys(coveredFiles).map((file) => ({
+      path: file.replace(dirPath, ''),
+      coverage: coveredFiles[file]
+    }))
+  } catch (e) {
+    console.error(`
             Coverage not found, please generate it using instanbul/nyc.
             We expect the json-summary format (coverage/coverage-summary.json).
             If you don't have any tests, you can still continue.
         `)
-    }
+  }
 
-    return coverage
+  return coverage
 }
--- src/options.ts
+++ src/options.ts
@@ -1,59 +1,59 @@
-import { CodehawkOptions, AllOptionKeys, AssembledOptions } from "./types"
+import { CodehawkOptions, AllOptionKeys, AssembledOptions } from './types'
 
 const baseOptions: CodehawkOptions = {
-    enableFlow: {
-        type: 'boolean',
-        default: false,
-        replaceDefault: true,
-    },
-    extensions: {
-        type: 'stringArray',
-        default: ['.js', '.jsx', '.ts', '.tsx'],
-        replaceDefault: false,
-    },
-    excludeDirectories: {
-        type: 'stringArray',
-        default: ['/dist', '/bin', '/build'],
-        replaceDefault: true,
-    },
-    excludeFilenames: {
-        type: 'stringArray',
-        default: ['.d.ts', '.min.js', '.bundle.js'],
-        replaceDefault: false,
-    },
-    skipDirectories: {
-        type: 'stringArray',
-        default: ['/node_modules', '/flow-typed', '/coverage'],
-        replaceDefault: false,
-    }
+  enableFlow: {
+    type: 'boolean',
+    default: false,
+    replaceDefault: true,
+  },
+  extensions: {
+    type: 'stringArray',
+    default: ['.js', '.jsx', '.ts', '.tsx'],
+    replaceDefault: false,
+  },
+  excludeDirectories: {
+    type: 'stringArray',
+    default: ['/dist', '/bin', '/build'],
+    replaceDefault: true,
+  },
+  excludeFilenames: {
+    type: 'stringArray',
+    default: ['.d.ts', '.min.js', '.bundle.js'],
+    replaceDefault: false,
+  },
+  skipDirectories: {
+    type: 'stringArray',
+    default: ['/node_modules', '/flow-typed', '/coverage'],
+    replaceDefault: false,
+  }
 }
 
 export const buildOptions = (
-    projectOptions: AssembledOptions
+  projectOptions: AssembledOptions
 ): AssembledOptions => {
-    const assembledOptions = {} as AssembledOptions
+  const assembledOptions = {} as AssembledOptions
 
-    Object.keys(baseOptions).forEach((optionKey: AllOptionKeys) => {
-        const option = baseOptions[optionKey]
-        let val = option.default
+  Object.keys(baseOptions).forEach((optionKey: AllOptionKeys) => {
+    const option = baseOptions[optionKey]
+    let val = option.default
 
-        if (projectOptions[optionKey]) {
-            // Project options can either be added to the defaults, or replace them.
-            if (option.replaceDefault) {
-                // Mutate options by replacing (we assume project config is valid!)
-                val = projectOptions[optionKey]
-            } else {
-                // Mutate options by mixing in project options to defaults
-                val = (option.type === 'stringArray' && Array.isArray(val))
-                    ? val.concat(projectOptions[optionKey] as Array<string>)
-                    : val = projectOptions[optionKey] as boolean
-            }
-        }
+    if (projectOptions[optionKey]) {
+      // Project options can either be added to the defaults, or replace them.
+      if (option.replaceDefault) {
+        // Mutate options by replacing (we assume project config is valid!)
+        val = projectOptions[optionKey]
+      } else {
+        // Mutate options by mixing in project options to defaults
+        val = (option.type === 'stringArray' && Array.isArray(val))
+          ? val.concat(projectOptions[optionKey] as string[])
+          : val = projectOptions[optionKey] as boolean
+      }
+    }
 
-        // TODO unsure how to remove this `any` atm
-        // It may require a rethink of the type defs
-        assembledOptions[optionKey] = val as any
-    })
+    // TODO unsure how to remove this `any` atm
+    // It may require a rethink of the type defs
+    assembledOptions[optionKey] = val as any
+  })
 
-    return assembledOptions as AssembledOptions
+  return assembledOptions
 }
