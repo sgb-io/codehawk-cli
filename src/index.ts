64697a07acdea4e707c4be9158771a2153124bfa
sgb-io <sam@sgb.io>
2020-11-11 22:23:04 +0000

    Add new CLI usage

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { analyzeProject, resultsAsTable } from './codehawk'
+
+// Sample CLI usage: `codehawk src`
+const scanDir = process.argv.slice(2);
+const output = analyzeProject(`${process.cwd()}/${scanDir}`)
+
+console.log(resultsAsTable(output.results))
